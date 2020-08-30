1657f970a252bc105f3d9d92d7830f31e2f2dd0b
GilgameshTC <travisching007@gmail.com>
2020-08-30 20:42:32 +0800

    Fix form validation
    Added route from registration to home page

--- app/containers/RegisterUser/index.js
+++ app/containers/RegisterUser/index.js
@@ -9,7 +9,7 @@
 import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import {
@@ -29,6 +29,7 @@ import makeSelectRegisterUser from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import ColorPallete from '../../colorPallete';
+import LogoWord from '../../components/LogoWord';
 import RegistrationConfirmationDialog from '../../components/RegistrationConfirmation';
 
 const formFields = {
@@ -58,13 +59,15 @@ export function RegisterUser({ history }) {
     history.push('/');
   }
 
+  // Returns null if field is valid
+  // Else returns validation message
   function validateFormFields(field) {
     switch (field) {
       case formFields.USERNAME:
-        return username === '' ? 'This field is required' : null;
+        return username === '' ? 'Username is required' : null;
       case formFields.EMAIL:
         if (email === '') {
-          return 'This field is required';
+          return 'Email is required';
         }
         const lastAtPos = email.lastIndexOf('@');
         const lastDotPos = email.lastIndexOf('.');
@@ -83,16 +86,16 @@ export function RegisterUser({ history }) {
         return null;
       case formFields.PASSWORD:
         if (password === '') {
-          return 'This field is required';
+          return 'Password is required';
         }
         if (password && password.length < 8) {
-          return 'Must be of 8 characters or more';
+          return 'Password must be of 8 characters or more';
         }
         if (
           password &&
           !/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/.test(password)
         ) {
-          return 'Need at least 1 number 1 alphabet';
+          return 'Password needs at least 1 number 1 alphabet';
         }
         return null;
       default:
@@ -100,96 +103,131 @@ export function RegisterUser({ history }) {
     }
   }
 
-  function validateBothPasswords() {
-    return password !== '' && password === retypePassword;
+  // Returns null if all form fields are valid
+  // Else returns validation message
+  function validateForm() {
+    const usernameValidation = validateFormFields(formFields.USERNAME);
+    if (usernameValidation) {
+      return usernameValidation;
+    }
+    const emailValidation = validateFormFields(formFields.EMAIL);
+    if (emailValidation) {
+      return emailValidation;
+    }
+    const passwordValidation = validateFormFields(formFields.PASSWORD);
+    if (passwordValidation) {
+      return passwordValidation;
+    }
+    if (password !== retypePassword) {
+      return "Passwords don't match";
+    }
+    return null;
   }
 
   return (
     <div
       style={{
         backgroundColor: ColorPallete.primaryColor,
-        width: '100vw',
-        height: '100vh',
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
       }}
     >
-      <Card
-        background="white"
-        width="40vw"
-        padding="2rem"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
+      <div
+        style={{
+          overlay: { zIndex: 10 },
+          position: 'absolute',
+        }}
       >
-        <Heading size={800}>Join VizBlocks</Heading>
-        <Strong size={500} marginTop="0.5rem">
-          {"Create projects, share ideas, make friends. It's free!"}
-        </Strong>
-        <TextInputField
-          width="80%"
-          marginTop="2rem"
-          label="Create your username"
-          placeholder="username"
-          required
-          validationMessage={validateFormFields(formFields.USERNAME)}
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <TextInputField
-          width="80%"
-          label="Email address"
-          placeholder="email"
-          required
-          validationMessage={validateFormFields(formFields.EMAIL)}
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <TextInputField
-          type={shouldDisplayPassword ? 'text' : 'password'}
-          width="80%"
-          label="Choose a password"
-          placeholder="password"
-          required
-          validationMessage={validateFormFields(formFields.PASSWORD)}
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <TextInputField
-          type={shouldDisplayPassword ? 'text' : 'password'}
-          width="80%"
-          label="Confirm password"
-          placeholder="Type password again"
-          required
-          validationMessage={
-            retypePassword === '' ? 'This field is required' : null
-          }
-          value={retypePassword}
-          onChange={e => setRetypePassword(e.target.value)}
-        />
-        <Checkbox
-          label="Show password"
-          checked={shouldDisplayPassword}
-          onChange={e => setShouldDisplayPassword(e.target.checked)}
-        />
-        <Button
-          onClick={() =>
-            validateBothPasswords()
-              ? setConfirmationIsShown(true)
-              : toaster.danger("Passwords don't match")
-          }
+        <Link to="/">
+          <LogoWord />
+        </Link>
+      </div>
+      <div
+        style={{
+          backgroundColor: ColorPallete.primaryColor,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Card
+          background="white"
+          width="40vw"
+          padding="2rem"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
         >
-          Create
-        </Button>
-      </Card>
-      <RegistrationConfirmationDialog
-        isShown={confirmationIsShown}
-        information={{ username, email }}
-        closeCallback={confirmationCloseCallback}
-        confirmCallback={confirmationSuccessCallback}
-      />
+          <Heading size={800}>Join VizBlocks</Heading>
+          <Strong size={500} marginTop="0.5rem">
+            {"Create projects, share ideas, make friends. It's free!"}
+          </Strong>
+          <TextInputField
+            width="80%"
+            marginTop="2rem"
+            label="Create your username"
+            placeholder="username"
+            required
+            validationMessage={validateFormFields(formFields.USERNAME)}
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <TextInputField
+            width="80%"
+            label="Email address"
+            placeholder="email"
+            required
+            validationMessage={validateFormFields(formFields.EMAIL)}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <TextInputField
+            type={shouldDisplayPassword ? 'text' : 'password'}
+            width="80%"
+            label="Choose a password"
+            placeholder="password"
+            required
+            validationMessage={validateFormFields(formFields.PASSWORD)}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <TextInputField
+            type={shouldDisplayPassword ? 'text' : 'password'}
+            width="80%"
+            label="Confirm password"
+            placeholder="Type password again"
+            required
+            validationMessage={
+              retypePassword === '' ? 'This field is required' : null
+            }
+            value={retypePassword}
+            onChange={e => setRetypePassword(e.target.value)}
+          />
+          <Checkbox
+            label="Show password"
+            checked={shouldDisplayPassword}
+            onChange={e => setShouldDisplayPassword(e.target.checked)}
+          />
+          <Button
+            onClick={() => {
+              const validation = validateForm();
+              return validation
+                ? toaster.danger(validation)
+                : setConfirmationIsShown(true);
+            }}
+          >
+            Create
+          </Button>
+        </Card>
+        <RegistrationConfirmationDialog
+          isShown={confirmationIsShown}
+          information={{ username, email }}
+          closeCallback={confirmationCloseCallback}
+          confirmCallback={confirmationSuccessCallback}
+        />
+      </div>
     </div>
   );
 }
